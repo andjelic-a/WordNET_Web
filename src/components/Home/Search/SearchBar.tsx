35a0fffe283d711dd137864da7bc89142453e3b6
Branko <dinicbranko007@gmail.com>
2024-04-12 22:32:59 +0200

    Rewrote wordSelected Function (Better Error Handling And Less Overhead While Finding WordId)

--- src/components/Home/Search/SearchBar.tsx
+++ src/components/Home/Search/SearchBar.tsx
@@ -24,22 +24,25 @@ function SearchBar() {
     }
 
     const wordSelected = (e: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
-        // In future add here to open page with associated words to this word (based on id -> get id from dataset)
-        console.log(e.currentTarget.dataset.key);
+        // Get and parse id from string to number
+        const selectedWordId: number | undefined = parseInt(e.currentTarget.dataset.id!);
 
-        const selectedWordId: string | undefined = e.currentTarget.dataset.key;
-        if (selectedWordId) {
-            // Find the selected word from the words state
-            const selectedWord: WordDTO | undefined = words.find((word: WordDTO) => word.wordid.toString() === selectedWordId);
-
-            if (selectedWord) {
-                const searchBarInput: HTMLInputElement | null = document.querySelector('.input-SearchBar');
-                if (searchBarInput) {
-                    // Set the input value to selected word's name
-                    searchBarInput.value = selectedWord.name;
-                }
-            }
+        // Find the selected word from the words state (based on id)
+        const selectedWord: WordDTO | undefined = words.find((word: WordDTO) => word.wordid === selectedWordId);
+        if (!selectedWord) {
+            console.error("Cant find selected word");
+            return;
         }
+
+        // Find search bar based on its class
+        const searchBarInput: HTMLInputElement | null = document.querySelector('.input-SearchBar');
+        if (!searchBarInput) {
+            console.error("Cant find search bar");
+            return;
+        }
+
+        // Set input of search bar to word name
+        searchBarInput.value = selectedWord.name;
     }
 
     return (
@@ -47,7 +50,7 @@ function SearchBar() {
             <input type="text" className='input-SearchBar' onKeyUp={(e) => searchForWords(e)} placeholder='gore, kuća, neprijatelj...' />
             <button type='submit'><img src='../../../../img/search.png'></img></button>
             <div className="suggestedWords">
-                {words.map((word) => (<p data-key={word.wordid} key={word.wordid} onClick={(e) => wordSelected(e)}>{word.name}</p>))}
+                {words.map((word) => (<p data-id={word.wordid} key={word.wordid} onClick={(e) => wordSelected(e)}>{word.name}</p>))}
             </div>
         </form>
     )
