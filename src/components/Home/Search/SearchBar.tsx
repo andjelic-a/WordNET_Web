f4fd3cb1cd1866f5c367ee4081983ba79f5d82d6
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-06-06 16:50:36 +0200

    Finish positioning for words-container

--- src/components/Home/Search/SearchBar.tsx
+++ src/components/Home/Search/SearchBar.tsx
@@ -1,90 +1,129 @@
-import React, { useContext, useState } from 'react';
-import '../../../css/HomeComponents.css';
-import { findAssociatedWordsForWord, findSimilarWord } from '../../../request_handler/ServerRequest';
-import { WordDTO } from '../../../types/Types';
-import { associatedWordsContext } from '../HomeContex';
+import React, { useContext, useState } from "react";
+import "../../../css/HomeComponents.css";
+import {
+  findAssociatedWordsForWord,
+  findSimilarWord,
+} from "../../../request_handler/ServerRequest";
+import { WordDTO } from "../../../types/Types";
+import { associatedWordsContext } from "../HomeContex";
 
 function SearchBar() {
-    const [words, setWords] = useState<WordDTO[]>([]);
-    const { setAssociatedWords } = useContext(associatedWordsContext);
+  const [words, setWords] = useState<WordDTO[]>([]);
+  const { setAssociatedWords } = useContext(associatedWordsContext);
 
-    const handleClick = () => {
-        // If there are no suggested words we alert user there is no suggested words
-        if (words.length === 0) {
-            alert("There is no suggested words");
-            return;
-        }
-
-        // If there is only one suggested word we take that words by using index
-        if (words.length === 1) {
-            setAssociatedWords(findAssociatedWordsForWord(words[0].wordid));
-            return;
-        }
-
-        // If there are more than one suggested word we take user input and try to find suggested word by name
-        const selectedWord: HTMLInputElement | null = document.querySelector('.input-SearchBar');
-        const suggestedWord: WordDTO | undefined = words.find((word) => word.name === selectedWord?.value);
-        if (!suggestedWord) {
-            alert("Entered word doesn't exist");
-            return;
-        }
-
-        // When word is found we take id and get associated words and refresh WordsPage to show new data
-        setAssociatedWords(findAssociatedWordsForWord(suggestedWord.wordid));
+  const handleClick = () => {
+    // If there are no suggested words we alert user there is no suggested words
+    if (words.length === 0) {
+      alert("There is no suggested words");
+      return;
     }
 
-    const searchForWords = (e: React.KeyboardEvent<HTMLInputElement> | React.FocusEvent<HTMLInputElement, Element>) => {
-        // Get current entered chars
-        const word = e.currentTarget.value;
-
-        // If word is empty, set words to empty array
-        if (word.trim() === '') {
-            setWords([]);
-            return;
-        }
-
-        // If word is not empty or white spaces, search for similar words
-        const similarWords = findSimilarWord(word);
-
-        // Update found words list
-        setWords(similarWords.slice(0, 5));
+    // If there is only one suggested word we take that words by using index
+    if (words.length === 1) {
+      setAssociatedWords(findAssociatedWordsForWord(words[0].wordid));
+      return;
     }
 
-    const wordSelected = (e: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
-        // Get and parse id from string to number
-        const selectedWordId: number | undefined = parseInt(e.currentTarget.dataset.id!);
-
-        // Find the selected word from the words state (based on id)
-        const selectedWord: WordDTO | undefined = words.find((word: WordDTO) => word.wordid === selectedWordId);
-        if (!selectedWord) {
-            console.error("Cant find selected word");
-            return;
-        }
-
-        // Find search bar based on its class
-        const searchBarInput: HTMLInputElement | null = document.querySelector('.input-SearchBar');
-        if (!searchBarInput) {
-            console.error("Cant find search bar");
-            return;
-        }
-
-        // Set input of search bar to word name
-        searchBarInput.value = selectedWord.name;
+    // If there are more than one suggested word we take user input and try to find suggested word by name
+    const selectedWord: HTMLInputElement | null =
+      document.querySelector(".input-SearchBar");
+    const suggestedWord: WordDTO | undefined = words.find(
+      (word) => word.name === selectedWord?.value
+    );
+    if (!suggestedWord) {
+      alert("Entered word doesn't exist");
+      return;
     }
 
-    return (
-        <>
-            <div className='search-bar'>
-                <input type="text" className='input-SearchBar' onKeyUp={(e) => searchForWords(e)} placeholder='горе, кућа, непријатељ...' />
-                <a href="#Words"><button type='button'><img src='../../../../img/search.png' alt='Search icon' draggable="false" onClick={handleClick} /></button></a>
-            </div>
-            <div className='suggested-words-relative'>
-                <div className="suggested-words">
-                    {words.map((word) => (<p data-id={word.wordid} key={word.wordid} onClick={(e) => wordSelected(e)}>{word.name}</p>))}
-                </div>
-            </div>
-        </>
-    )
+    // When word is found we take id and get associated words and refresh WordsPage to show new data
+    setAssociatedWords(findAssociatedWordsForWord(suggestedWord.wordid));
+  };
+
+  const searchForWords = (
+    e:
+      | React.KeyboardEvent<HTMLInputElement>
+      | React.FocusEvent<HTMLInputElement, Element>
+  ) => {
+    // Get current entered chars
+    const word = e.currentTarget.value;
+
+    // If word is empty, set words to empty array
+    if (word.trim() === "") {
+      setWords([]);
+      return;
+    }
+
+    // If word is not empty or white spaces, search for similar words
+    const similarWords = findSimilarWord(word);
+
+    // Update found words list
+    setWords(similarWords.slice(0, 5));
+  };
+
+  const wordSelected = (
+    e: React.MouseEvent<HTMLParagraphElement, MouseEvent>
+  ) => {
+    // Get and parse id from string to number
+    const selectedWordId: number | undefined = parseInt(
+      e.currentTarget.dataset.id!
+    );
+
+    // Find the selected word from the words state (based on id)
+    const selectedWord: WordDTO | undefined = words.find(
+      (word: WordDTO) => word.wordid === selectedWordId
+    );
+    if (!selectedWord) {
+      console.error("Cant find selected word");
+      return;
+    }
+
+    // Find search bar based on its class
+    const searchBarInput: HTMLInputElement | null =
+      document.querySelector(".input-SearchBar");
+    if (!searchBarInput) {
+      console.error("Cant find search bar");
+      return;
+    }
+
+    // Set input of search bar to word name
+    searchBarInput.value = selectedWord.name;
+  };
+
+  return (
+    <>
+      <div className="search-bar">
+        <input
+          type="text"
+          className="input-SearchBar"
+          onKeyUp={(e) => searchForWords(e)}
+          placeholder="горе, кућа, непријатељ..."
+        />
+        <a href="#Words">
+          <button type="button">
+            <img
+              src="../../../../img/search.png"
+              alt="Search icon"
+              draggable="false"
+              onClick={handleClick}
+            />
+          </button>
+        </a>
+      </div>
+      <div className="suggested-words-relative">
+        <div className="suggested-words">
+          {words.map((word) => (
+            <p
+              data-id={word.wordid}
+              key={word.wordid}
+              onClick={(e) => wordSelected(e)}
+            >
+              {word.name}
+            </p>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SearchBar;
--- src/components/Home/Words/Words.tsx
+++ src/components/Home/Words/Words.tsx
@@ -1,28 +1,40 @@
 import { useContext } from "react";
 import { words } from "../../../request_handler/ServerRequest";
 import { associatedWordsContext } from "../HomeContex";
+import "../../../css/WordsComponent.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 function Words() {
-    const { associatedWords } = useContext(associatedWordsContext);
+  const { associatedWords } = useContext(associatedWordsContext);
+  let associatedWordCounter = 1;
 
-    const test = () => {
-        if (associatedWords.length < 1) {
-            return (<></>);
-        }
-        
-        return (
-            <div id="Words" className="words">
-                <div className="words-header">
-                    <h1>{(words.find((word) => word.wordid === associatedWords[0].wordid))?.name.toUpperCase()}</h1>
-                </div>
-                {associatedWords.map((word) => <p key={word.associatedwordid}>{word.name}</p>)}
-            </div>
-        )
+  const test = () => {
+    if (associatedWords.length < 1) {
+      return <></>;
     }
 
     return (
-        <>{test()}</>
-    )
+      <div id="Words" className="words-page">
+        <div className="words-container">
+          <div className="words-header">
+            <h1>
+              {words
+                .find((word) => word.wordid === associatedWords[0].wordid)
+                ?.name.toUpperCase()}
+            </h1>
+          </div>
+          <div className="associated-word-container">
+            {associatedWords.map((word) => (
+              <div className="associated-word"><div className="associated-word-circle">{associatedWordCounter++}</div><p key={word.associatedwordid}>{word.name}</p></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return <>{test()}</>;
 }
 
 export default Words;
