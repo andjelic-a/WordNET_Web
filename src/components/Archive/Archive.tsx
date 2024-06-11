9245e442c871cd9a740dd662b0610e118705363f
Branko <dinicbranko007@gmail.com>
2024-06-11 20:02:13 +0200

    Archives Now Show All Words With Their Associated Words

--- src/components/Archive/Archive.tsx
+++ src/components/Archive/Archive.tsx
@@ -1,314 +1,39 @@
 import "../../css/ArchiveComponents.css";
+import { words } from "../../request_handler/ServerRequest.ts";
 import Header from "../Home/Header/Header.tsx";
 
 function Archive() {
+  console.log(words)
+
   return (
     <div className="archive-component">
       <div className="archive-header">
         <Header />
       </div>
+
       <div className="grid-container">
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
+        {words.map((w) => (
+          <div className="grid-item">
+            <div className="words-container">
+
+              <div className="words-header">
+                <h1>{w.name}</h1>
               </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
+
+              <div className="associated-word-container">
+                {w.associatedwords.map((a) => (
+                  <div className="associated-word">
+                    <div className="associated-word-circle">{a.count}</div>
+                    <p>{a.name}</p>
+                  </div>
+                ))}
               </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
+
             </div>
           </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grid-item">
-          <div className="words-container">
-            <div className="words-header">
-              <h1>Adasdasd</h1>
-            </div>
-            <div className="associated-word-container">
-              <div className="associated-word">
-                <div className="associated-word-circle">1</div>
-                <p>dasdads</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">2</div>
-                <p>33333333333</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">3</div>
-                <p>dasd3123123</p>
-              </div>
-              <div className="associated-word">
-                <div className="associated-word-circle">4</div>
-                <p>3124515dasdad</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
+
     </div>
   );
 }
