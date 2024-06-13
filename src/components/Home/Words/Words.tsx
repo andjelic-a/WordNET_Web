d0828de5109421e9b1d7fd90f8bf44d473b1f76d
Branko <dinicbranko007@gmail.com>
2024-06-13 16:03:24 +0200

    Rewrote Words.tsx Better Sorting Method

--- src/components/Home/Words/Words.tsx
+++ src/components/Home/Words/Words.tsx
@@ -1,47 +1,49 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { words } from "../../../request_handler/ServerRequest";
 import { associatedWordsContext } from "../HomeContex";
 import "../../../css/WordsComponent.css";
 
 function Words() {
   const { associatedWords } = useContext(associatedWordsContext);
-  let associatedWordCounter = 1;
 
-  const test = () => {
-    if (associatedWords.length < 1) {
-      return <></>;
-    }
-
-    const wordClick = (e: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
-      console.log(JSON.parse(e.currentTarget.dataset.statistics!));
-    }
-
-    return (
-      <div id="Words" className="words-page">
-        <div className="words-container">
-          <div className="words-header">
-            <h1>
-              {words
-                .find((word) => word.Id === associatedWords[0].WordId)
-                ?.Name.toUpperCase()}
-            </h1>
-          </div>
-          <div className="associated-word-container">
-            {associatedWords.sort((a, b) => b.Count - a.Count).map((word) => (
-              <div key={associatedWordCounter++} className="associated-word">
-                <div className="associated-word-circle">
-                  {word.Count}
-                </div>
-                <p data-statistics={JSON.stringify(word.Statistics)} key={word.Id} onClick={(e) => wordClick(e)}>{word.Name}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    );
+  const wordClick = (e: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
+    console.log(JSON.parse(e.currentTarget.dataset.statistics!));
   };
 
-  return <>{test()}</>;
+  const sortedAssociatedWords = useMemo(() => {
+    return associatedWords.sort((a, b) => b.Count - a.Count);
+  }, [associatedWords]);
+
+  if (associatedWords.length < 1) {
+    return null;
+  }
+
+  const wordName = words.find((word) => word.Id === associatedWords[0].WordId)?.Name.toUpperCase();
+
+  return (
+    <div id="Words" className="words-page">
+      <div className="words-container">
+        <div className="words-header">
+          <h1>{wordName}</h1>
+        </div>
+        <div className="associated-word-container">
+          {sortedAssociatedWords.map((word) => (
+            <div key={word.Id} className="associated-word">
+              <div className="associated-word-circle">
+                {word.Count}
+              </div>
+              <p
+                data-statistics={JSON.stringify(word.Statistics)}
+                onClick={wordClick}
+              >
+                {word.Name}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Words;
