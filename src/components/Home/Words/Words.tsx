0224f7532da258ebff6a1ff4dec3a1fd4b2c8228
Aleksa Andjelic <andjelic06aleksa@gmail.com>
2024-06-15 16:20:14 +0200

    Create new div element when word is active

--- src/components/Home/Words/Words.tsx
+++ src/components/Home/Words/Words.tsx
@@ -7,16 +7,22 @@ function Words() {
   const { associatedWords } = useContext(associatedWordsContext);
   const { word } = useContext(wordContext);
   const [activeWord, setActiveWord] = useState<number | null>(null);
+  const [animate, setAnimate] = useState(false);
+  const [showNewContainer, setShowNewContainer] = useState(false);
   const wordRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   const wordClick = (statistics: Questionee[], index: number) => {
     setActiveWord(index);
+    setAnimate(true);
+    setShowNewContainer(true);
     console.log(statistics);
   };
 
   const handleClickOutside = (event: MouseEvent) => {
     if (wordRefs.current.every((ref) => ref && !ref.contains(event.target as Node))) {
       setActiveWord(null);
+      setAnimate(false);
+      setShowNewContainer(false);
     }
   };
 
@@ -35,29 +41,34 @@ function Words() {
     return null;
   }
 
-  //const wordName = words.find((word) => word.Id === associatedWords[0].WordId)?.Name.toUpperCase();
   const wordName = word.toUpperCase();
 
   return (
     <div id="Words" className="words-page">
-      <div className="words-container">
-        <div className="words-header">
-          <h1>{wordName}</h1>
+      <div className={`words-container-wrapper ${animate ? 'animate' : ''}`}>
+        <div className="words-container">
+          <div className="words-header">
+            <h1>{wordName}</h1>
+          </div>
+          <div className="associated-word-container">
+            {sortedAssociatedWords.map((word, index) => (
+              <div
+                key={word.id}
+                className={`associated-word ${activeWord === index ? "active" : ""}`}
+                ref={(el) => (wordRefs.current[index] = el)}
+              >
+                <div className="associated-word-circle">{word.count}</div>
+                <p onClick={() => wordClick(word.questioneeDTOs, index)}>
+                  {word.name}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="associated-word-container">
-          {sortedAssociatedWords.map((word, index) => (
-            <div
-              key={word.id}
-              className={`associated-word ${activeWord === index ? "active" : ""}`}
-              ref={(el) => (wordRefs.current[index] = el)}
-            >
-              <div className="associated-word-circle">{word.count}</div>
-              <p onClick={() => wordClick(word.questioneeDTOs, index)}>
-                {word.name}
-              </p>
-            </div>
-          ))}
-        </div>
+        {showNewContainer && (
+          <div className="new-words-container">
+          </div>
+        )}
       </div>
     </div>
   );
