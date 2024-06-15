8fb9557402475e3d3abdeb22ff4151a99ab920d6
Branko Car <dinicbranko007@gmail.com>
2024-06-15 18:02:54 +0200

    Added Graphs To Words Page

--- src/components/Home/Words/Words.tsx
+++ src/components/Home/Words/Words.tsx
@@ -2,6 +2,7 @@ import { useContext, useMemo, useState, useEffect, useRef } from "react";
 import { associatedWordsContext, wordContext } from "../HomeContex";
 import "../../../css/WordsComponent.css";
 import { Questionee } from "../../../types/Types";
+import { BarChart, PieChart } from "@mui/x-charts";
 
 function Words() {
   const { associatedWords } = useContext(associatedWordsContext);
@@ -10,6 +11,19 @@ function Words() {
   const [animate, setAnimate] = useState(false);
   const [showNewContainer, setShowNewContainer] = useState(false);
   const wordRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  const [statistics, setStatistics] = useState<{
+    gender: {
+      manPerc: number,
+      womanPerc: number
+    },
+
+    age: {
+      young: number,
+      middle: number,
+      old: number
+    }
+  } | null>(null)
 
   const wordClick = (statistics: Questionee[], index: number) => {
     setActiveWord(index);
@@ -20,6 +34,21 @@ function Words() {
 
     const manPerc = statistics.filter(q => q.isMan == true).length / statistics.length;
     const womanPerc = statistics.filter(q => q.isMan == false).length / statistics.length;
+
+    const young = statistics.filter(q => q.age > 0 && q.age < 18).length;
+    const middle = statistics.filter(q => q.age > 18 && q.age < 25).length;
+    const old = statistics.filter(q => q.age > 25).length;
+
+    setStatistics({
+      gender: {
+        manPerc,
+        womanPerc
+      }, age: {
+        young,
+        middle,
+        old
+      }
+    });
 
     console.log("Percentage: ");
     console.log(`Man => ${(manPerc * 100).toFixed(2)}%`);
@@ -79,6 +108,37 @@ function Words() {
             ))}
           </div>
         </div>
+
+        {statistics && (
+          <PieChart
+            series={[
+              {
+                data: [
+                  { color: 'blue', value: statistics.gender.manPerc, label: 'Man' },
+                  { color: 'pink', value: statistics.gender.womanPerc, label: 'Woman' }
+                ],
+                arcLabel: (item) => `${item.value * 100}%`,
+                arcLabelMinAngle: 0,
+                highlightScope: { faded: 'global', highlighted: 'item' },
+                faded: { innerRadius: 10, additionalRadius: -10, color: 'gray' },
+              },
+            ]}
+            width={400}
+            height={200}
+          />
+        )}
+
+        {statistics && (
+          <BarChart
+            series={[
+              { data: [statistics.age.young, statistics.age.middle, statistics.age.old] },
+            ]}
+            height={290}
+            xAxis={[{ data: ['0-18', '18-25', '25-♾️'], scaleType: 'band' }]}
+            margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+          />
+        )}
+
         {showNewContainer && (
           <div className="new-words-container">
           </div>
